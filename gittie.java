c21e3ab633362fa96810ac7b11e57da7581d9078
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-11 09:21:05 +0100

    joke functions is done

--- gittie.java
+++ gittie.java
@@ -4,7 +4,12 @@ public class Gittie {
         System.out.println("Hello, I'm Gittie!");
     }
 
+
+    public void joke() {
+        System.out.println("It's a jokeeeeee!");
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
