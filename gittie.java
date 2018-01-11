1335113d6c3b292a8f9d19398236299ecaf8c043
peti120 <peti59550@gmail.com>
2018-01-11 09:36:09 +0100

    merge accepted

--- gittie.java
+++ gittie.java
@@ -1,13 +1,10 @@
 public class Gittie {
 
     public void introduce() {
-<<<<<<< HEAD
         String name = "Gittie";
         System.out.println("Hello, " + name);
-=======
         Sytem.out.println("Cső te$ám");
         Sytem.out.println("Hello bro");
->>>>>>> 79e7d6244bf0d4ab0b4917b17b0b162558b35af6
     }
 
 
